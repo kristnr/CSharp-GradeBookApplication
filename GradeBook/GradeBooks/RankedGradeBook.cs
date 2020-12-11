f102771c7914923d98d1dcd5a5964227ef2b9e1b
kristnr <48805849+kristnr@users.noreply.github.com>
2020-12-11 13:30:12 -0500

    Fix GradeBookTypes

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,11 +4,24 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook (string name) : base (name)
         {
             base.Type = Enums.GradeBookType.Ranked;
+        }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
         }
     }
 }
