9b227d27eb2ce7424384089305519d07b788d9b9
kristnr <48805849+kristnr@users.noreply.github.com>
2020-12-11 13:44:19 -0500

    More GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -8,7 +10,7 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook (string name) : base (name)
         {
-            Type = Enums.GradeBookType.Ranked;
+            Type = GradeBookType.Ranked;
         }
 
         private string _name;
@@ -28,6 +30,19 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5) {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+            var threshold = (int)Math.Ceiling(Students.Count * 2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+            {
+                return 'A'
+            } else if (grades[(threshold*2) - 1] <= averageGrade) {
+                return 'B';
+            } else if (grades[(threshold*3) - 1] <= averageGrade) {
+                return 'C';
+            } else if (grades[(threshold*4) - 1] <= averageGrade) {
+                return 'D';
             }
             return 'F';
         }
