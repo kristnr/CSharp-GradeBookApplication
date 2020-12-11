498f95d1f27dbaf61250d12edfcbccb4e9a15981
kristnr <48805849+kristnr@users.noreply.github.com>
2020-12-11 13:28:04 -0500

    New GradeBookTypes

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base (name)
+        {
+            base.Type = Enums.GradeBookType.Standard;
+        }
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+    }
+}
